e0466e7e46ecc1a96936c692b519768b97720307
ArkashaJavelin <stepanov.arkadyushka@inbox.ru>
2022-08-15 12:26:52 +0400

    It's working now...

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::process::Command;
+
 fn main() {
-    println!("Hello, world!");
+    let new_command = Command::new("lxc").arg("--version").output().unwrap_or_else(|e| {
+      panic!("Command had been failed {}", e);
+    });
+
+    let str = String::from_utf8_lossy(&new_command.stdout);
+
+    print!("{}", str);
 }
